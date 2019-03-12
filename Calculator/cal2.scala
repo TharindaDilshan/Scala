48a9131143213c5ba0cb4160eb7d4a7310e383b6
JayaniH <43147809+JayaniH@users.noreply.github.com>
2019-03-12 23:26:28 +0530

    Update cal2.scala

--- Calculator/cal2.scala
+++ Calculator/cal2.scala
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-object cal2{
-    def main(args: Array[String]){
-        var n = 0.0;
-        var op = "0";
-        var total = 0.0;
-
-        println("Enter expression:");
-        total = scala.io.StdIn.readLine.toInt;
-
-        while(true){
-            var op = scala.io.StdIn.readLine;
-            
-            if (op == "="){
-                println ("Total = " + total);
-                System.exit(1);
-            }
-
-            n = scala.io.StdIn.readLine.toFloat;
-            op match{
-                case "+" => total = total + n;
-                case "-" => total = total - n;
-                case "*" => total = total * n;
-                case "/" => total = total / n;
-                case _ => println("Undefined operator");
-            }
-        }
-    }
-=======
 object cal2{
     def main(args: Array[String]){
         var n = 0;
@@ -54,5 +25,4 @@ object cal2{
             }
         }
     }
->>>>>>> 0cb6fe346bc9c01a33a1aee32e1531e3b25fdbe3
 }
