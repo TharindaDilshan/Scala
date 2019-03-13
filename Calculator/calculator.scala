aa9b7d3e1ac76bc7dc978d1c57b0b2101e12ce6b
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2019-03-13 19:17:49 +0530

    Calculator updated

--- Calculator/calculator.scala
+++ Calculator/calculator.scala
@@ -1,26 +1,30 @@
 object cal{
     def main(args:Array[String]){
-        var total=0.0;
+        
+        var total=scala.io.StdIn.readLine("Enter Expression:\n").toDouble;
         var num2=0.0;
         var op ="0";
-        println("Enter Expression: ");
+        
         while(true){
             var in = scala.io.StdIn.readLine;
             if(in=="="){
-                total=total+num2;
+                //total=total+num2;
                 total=(total-(total%0.01));
                 println("Value: "+total);
                 System.exit(1);
-            }else{
-                in match{
-                    case "+" => total=total+num2;
-                    case "-" => total=total-num2;
-                    case "*" => total=total*num2;
-                    case "/" => total=total/num2;
-                    case _ => num2=in.toFloat;
-                }
-                
             }
+            var num2=scala.io.StdIn.readLine.toDouble;
+            
+            
+            in match{
+                case "+" => total=total+num2;
+                case "-" => total=total-num2;
+                case "*" => total=total*num2;
+                case "/" => total=total/num2;
+                case _ => println("Invalid operator");
+            }
+                
+            
         }
     }
 }
